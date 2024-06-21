8dc5fecc2743d72077275873795fa8114c1f23a1
Henry-Monterroza <henry.monterroza@outlook.com>
2024-06-20 19:38:07 -0600

    Ocr Modulo

--- recursos/js/masterArrayToXLS.js
+++ recursos/js/masterArrayToXLS.js
@@ -1,135 +1,104 @@
-﻿// Array de objetos
-const ejemploArray = [
-    {
-        "nombre": "Juan",
-        "edad": 30,
-        "ciudad": "Madrid"
-    },
-    {
-        "nombre": "María",
-        "edad": 25,
-        "ciudad": "Barcelona"
-    },
-    {
-        "nombre": "Pedro",
-        "edad": 35,
-        "ciudad": "Valencia"
-    }
-];
-
+﻿
 $(document).ready(function () {
-    // Convertir el array a una cadena JSON
-    const jsonString = JSON.stringify(ejemploArray, null, 4); // Formateado con indentación
-
-    // Asignar la cadena JSON al placeholder del campo de texto
-    $("#textarray").attr("placeholder", jsonString);
-
-    $("#ArrayEjemplo").val(`[
-        {
-            "id": 1,
-            "nombre": "Juan",
-            "edad": 28,
-            "ciudad": "Madrid",
-            "ocupacion": "Ingeniero"
-        },
-        {
-            "id": 2,
-            "nombre": "María",
-            "edad": 34,
-            "ciudad": "Barcelona",
-            "ocupacion": "Diseñadora"
-        },
-        {
-            "id": 3,
-            "nombre": "Carlos",
-            "edad": 22,
-            "ciudad": "Valencia",
-            "ocupacion": "Estudiante"
-        },
-        {
-            "id": 4,
-            "nombre": "Ana",
-            "edad": 45,
-            "ciudad": "Sevilla",
-            "ocupacion": "Médico"
-        },
-        {
-            "id": 5,
-            "nombre": "Luis",
-            "edad": 30,
-            "ciudad": "Zaragoza",
-            "ocupacion": "Abogado"
-        }
-    ]`);
-
-
-    $('#copyButton').click(function() {
-    let textArea = $("#ArrayEjemplo");
-
-    textArea.prop("disabled",false);
-        textArea.focus();
-        textArea.select();
-        try {
-            textArea.select();
-            const success = document.execCommand('copy');
-            console.log(`Text copy was ${success ? 'successful' : 'unsuccessful'}.`);
-          } catch (err) {
-            console.error(err.name, err.message);
-          }
-
-          textArea.prop("disabled",true);
-
-    });
-
-
-    // Evento click del botón exportar
-    $("#exportBtn").click(async function () {
-        let stringarray = $("#textarray").val();
-        let filename = $("#filename").val();
-
-        // Validar campos
-        if (isNullOrEmpty(filename)) {
-            mostrarAlerta("Ingrese un nombre para el archivo", "#filename");
-            return;
-        }
-
-        if (isNullOrEmpty(stringarray)) {
-            mostrarAlerta("Ingrese un array en formato JSON", "#textarray");
-            return;
-        }
-
-        $("#exportBtn").prop('disabled', true);
-
-        if (esJsonArray(stringarray)) {
-            let data = JSON.parse(stringarray);
-            TabularJson(data);
-        } else {
-            mostrarAlerta("El texto ingresado no es un array JSON válido", "#textarray");
-        }
-
-        // Inicializar los campos y botones al cargar la página
-        $("#exportBtn").prop('disabled', false);
-        $("#filename").val(null);
-    });
-
-    function mostrarAlerta(mensaje, selector) {
-        
-        let alerty = AlertDiv(mensaje);
-        $("#msjAlert").html(alerty);
-        $("#exportBtn").prop('disabled', false);
-        $(selector).focus();
-    }
-
-    function isNullOrEmpty(value) {
-        return !value || value.trim() === "";
-    }
-
-    function esJsonArray(str) {
-        try {
-            let data = JSON.parse(str);
-            return Array.isArray(data);
-        } catch (e) {
-            return false;
-        }
-    }
+    $("#textarray").attr("placeholder", getPlaceholderData());
+    $("#ArrayEjemplo").val(getSampleData());
+    $('#copyButton').click(handleCopyButtonClick);
+    $("#exportBtn").click(handleExportButtonClick);
 });
+
+
+// function handleCopyButtonClick() {
+//     let textArea = $("#ArrayEjemplo");
+//     textArea.prop("disabled", false);
+//     textArea.focus();
+//     textArea.select();
+//     try {
+//         let success = document.execCommand('copy');
+//         console.log(`Text copy was ${success ? 'successful' : 'unsuccessful'}.`);
+//     } catch (err) {
+//         console.error(err.name, err.message);
+//     }
+//     textArea.prop("disabled", true);
+// }
+
+function handleCopyButtonClick() {
+    let textArea = $("#ArrayEjemplo");
+    textArea.prop("disabled", false);
+
+    // Obtener el texto del área de texto
+    const textToCopy = textArea.val();
+    textArea.select();// solo para que se vea selecionado.
+    // Copiar el texto al portapapeles usando Clipboard API
+    navigator.clipboard.writeText(textToCopy).then(function() {
+        console.log('Texto copiado al portapapeles con éxito.');
+    }).catch(function(err) {
+        console.error('Error al copiar texto al portapapeles: ', err);
+    });
+
+    textArea.prop("disabled", true);
+}
+
+
+
+async function handleExportButtonClick() {
+    let stringArray = $("#textarray").val();
+    let filename = $("#filename").val();
+
+    if (isNullOrEmpty(filename)) {
+        mostrarAlerta("Ingrese un nombre para el archivo", "#filename");
+        return;
+    }
+
+    if (isNullOrEmpty(stringArray)) {
+        mostrarAlerta("Ingrese un array en formato JSON", "#textarray");
+        return;
+    }
+
+    $("#exportBtn").prop('disabled', true);
+
+    if (esJsonArray(stringArray)) {
+        let data = JSON.parse(stringArray);
+        TabularJson(data);
+    } else {
+        mostrarAlerta("El texto ingresado no es un array JSON válido", "#textarray");
+    }
+
+    $("#exportBtn").prop('disabled', false);
+    $("#filename").val(null);
+}
+
+function mostrarAlerta(mensaje, selector) {
+    let alertMessage = AlertDiv(mensaje);
+    $("#msjAlert").html(alertMessage);
+    $("#exportBtn").prop('disabled', false);
+    $(selector).focus();
+}
+
+
+function esJsonArray(str) {
+    try {
+        const data = JSON.parse(str);
+        return Array.isArray(data);
+    } catch (e) {
+        return false;
+    }
+}
+
+function getSampleData() {
+    return JSON.stringify([
+        { "id": 1, "nombre": "Juan", "edad": 28, "ciudad": "Madrid", "ocupacion": "Ingeniero" },
+        { "id": 2, "nombre": "María", "edad": 34, "ciudad": "Barcelona", "ocupacion": "Diseñadora" },
+        { "id": 3, "nombre": "Carlos", "edad": 22, "ciudad": "Valencia", "ocupacion": "Estudiante" },
+        { "id": 4, "nombre": "Ana", "edad": 45, "ciudad": "Sevilla", "ocupacion": "Médico" },
+        { "id": 5, "nombre": "Luis", "edad": 30, "ciudad": "Zaragoza", "ocupacion": "Abogado" }
+    ], null, 4);
+}
+
+function getPlaceholderData(){
+    return JSON.stringify([
+        { "nombre": "Juan", "edad": 30, "ciudad": "Madrid" },
+        { "nombre": "María", "edad": 25, "ciudad": "Barcelona" },
+        { "nombre": "Pedro", "edad": 35, "ciudad": "Valencia" }
+    ], null, 4); // Formateado con indentación
+
+}
